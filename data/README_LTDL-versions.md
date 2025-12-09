ee97062d6b779f26a99fd6fe151f91a514cdf989
Lia Ossanna <lossanna@arizona.edu>
2025-12-09 16:55:15 -0700

    Deal with Init_Date and Comp_Date for treatment info table

--- data/README_LTDL-versions.md
+++ data/README_LTDL-versions.md
@@ -1,7 +1,7 @@
 # README for `data/LTDL-versions/`
 
 Created: 2025-12-04  
-Updated: 2025-12-04
+Updated: 2025-12-09
 
 ## Notes
 - This file is kept in the `data/` folder and not the `data/LTDL-versions/` folder because the latter is not pushed to GitHub, but I would like to have version control for the README, which will document all the data cleaning changes for the LTDL data.
@@ -14,9 +14,13 @@ Updated: 2025-12-04
 
 ### v0.0.0
 - All rows from original data; only thing changed is that the problems caused by overflow cells have been corrected for `Project_Info` and `Treatment_Info` tables.
+- CSVs ending in `_v0.0.0gisjoin` are versions with only a few selected columns, intended to be loaded into ArcGIS and joined to LTDL point/polygon/line features to append some useful data to the features (makes it easier to understanding what is going on when viewing a pop-up window).
 
+### v0.0.1
 
 
 ## Directory
 - `01_Project-info_v0.0.0.csv` (created 2025-12-04)
+- `01_Project-info_v0.0.0gisjoin.csv` (created 2025-12-09)
 - `01_Treatment-info_v0.0.0.csv` (created 2025-12-04)
+- `01_Treatment-info_v0.0.0gisjoin.csv` (created 2025-12-09)
--- scripts/05.1_treatment-info_v0.0.1.R
+++ scripts/05.1_treatment-info_v0.0.1.R
@@ -1,9 +1,12 @@
 # Created: 2025-12-04
-# Updated: 2025-12-05
+# Updated: 2025-12-09
 
 # Purpose: Complete initial data cleaning to create Treatment_Info v0.0.1.
 
-# Filter for polygons, implemented plans, confirmed features only.
+# Data cleaning:
+#   Filter for polygons, implemented plans, confirmed features only.
+#   Clean dates (Init_Date, Comp_Date).
+
 
 library(tidyverse)
 
@@ -45,13 +48,53 @@ count(init.date, init_m)
 count(init.date, init_d) %>% 
   print(n = 33)
 
+# Check 0s
+init.date0 <- init.date %>% 
+  filter(init_d == "0" | init_m == "0") # 0 is not mixed with 00
+
 # Create estimate month and day for 00 (unknown)
 init.date <- init.date %>% 
   mutate(init_m_est = case_when(
-    init_m == "00" ~ "06",
-    TRUE ~ init_m
-  ))
+    init_m == "00" ~ "6",
+    init_m == "0" ~ "6",
+    TRUE ~ init_m)) %>% 
+  mutate(init_d_est = case_when(
+    init_d == "00" ~ "15",
+    init_d == "0" ~ "15",
+    TRUE ~ init_d)) %>% 
+  mutate(init_est = case_when(
+    init_m == "00" & init_d == "00" ~ "estimated month and day",
+    init_m == "0" & init_d == "0" ~ "estimated month and day",
+    init_m == "00" ~ "estimated month",
+    init_m == "0" ~ "estimated month",
+    init_d == "00" ~ "estimated day",
+    init_d == "0" ~ "estimated day",
+    TRUE ~ "actual")) %>%
+  mutate(init_m_est = str_pad(init_m_est, width = 2, pad = "0")) %>% 
+  mutate(init_d_est = str_pad(init_d_est, width = 2, pad = "0")) %>% 
+  mutate(init_date_est = paste0(init_y, "-", init_m_est, "-", init_d_est)) %>% 
+  mutate(init_date_est = as.Date(init_date_est))
 
+# Look for NA dates
+init.date.na <- init.date %>% 
+  filter(is.na(init_date_est)) # NAs created because of invalid dates for the month
+
+# Adjust date to fit month for NAs
+init.date.na.fix <- init.date.na %>% 
+  mutate(init_d_est = c("30", "30", "28", "28", "30", "20"),
+         init_est = rep("estimated day", nrow(init.date.na))) %>% 
+  mutate(init_date_est = paste0(init_y, "-", init_m_est, "-", init_d_est)) %>% 
+  mutate(init_date_est = as.Date(init_date_est))
+
+# Replace fixed NA estimated dates
+init.date <- init.date %>% 
+  filter(!is.na(init_date_est)) %>% 
+  bind_rows(init.date.na.fix) %>% 
+  arrange(Trt_ID)
+
+# Join with treatment.info
+treatment.info <- treatment.info %>% 
+  left_join(init.date)
 
 
 # Comp_Date ---------------------------------------------------------------
@@ -69,7 +112,171 @@ count(comp.date, comp_m)
 count(comp.date, comp_d) %>% 
   print(n = 33)
 
+# Check 0s
+comp.date0 <- comp.date %>% 
+  filter(comp_d == "0" | comp_m == "0") # 0 is not mixed with 00
+
+# Create estimate month and day for 00 (unknown)
+comp.date <- comp.date %>% 
+  mutate(comp_m_est = case_when(
+    comp_m == "00" ~ "6",
+    comp_m == "0" ~ "6",
+    TRUE ~ comp_m)) %>% 
+  mutate(comp_d_est = case_when(
+    comp_d == "00" ~ "15",
+    comp_d == "0" ~ "15",
+    TRUE ~ comp_d)) %>% 
+  mutate(comp_est = case_when(
+    comp_m == "00" & comp_d == "00" ~ "estimated month and day",
+    comp_m == "0" & comp_d == "0" ~ "estimated month and day",
+    comp_m == "00" ~ "estimated month",
+    comp_m == "0" ~ "estimated month",
+    comp_d == "00" ~ "estimated day",
+    comp_d == "0" ~ "estimated day",
+    TRUE ~ "actual")) %>%
+  mutate(comp_m_est = str_pad(comp_m_est, width = 2, pad = "0")) %>% 
+  mutate(comp_d_est = str_pad(comp_d_est, width = 2, pad = "0")) %>% 
+  mutate(comp_date_est = paste0(comp_y, "-", comp_m_est, "-", comp_d_est)) %>% 
+  mutate(comp_date_est = as.Date(comp_date_est))
+
+# Look for NA dates
+comp.date.na <- comp.date %>% 
+  filter(is.na(comp_date_est)) 
+count(comp.date.na, comp_m_est)
+count(comp.date.na, comp_d_est) # NAs created because of invalid dates for the month,
+#                                   except in one instance, where there is no year
+
+# Adjust date to fit month for NAs
+comp.date.na.fix <- comp.date.na %>% 
+  filter(Trt_ID != 73162) %>% 
+  mutate(comp_d_est = case_when(
+    comp_m_est == "02" ~ "28",
+    TRUE ~ "30")) %>% 
+  mutate(comp_est = rep("estimated day", nrow(comp.date.na) - 1)) %>% 
+  mutate(comp_date_est = paste0(comp_y, "-", comp_m_est, "-", comp_d_est)) %>% 
+  mutate(comp_date_est = as.Date(comp_date_est))
+
+# Investigate Trt_ID 73162 (missing year completed)
+trtid.73162 <- treatment.info %>% 
+  filter(Trt_ID == 73162)
+
+#   Look at dates of other prescribed burns
+prescribed.burn <- treatment.info %>% 
+  filter(Trt_Type_Sub == "Prescribed Burn") %>% 
+  separate(Init_Date, into = c("init_m", "init_d", "init_y"), sep = "/", remove = FALSE) %>% 
+  separate(Comp_Date, into = c("comp_m", "comp_d", "comp_y"), sep = "/", remove = FALSE)
+
+#   See that most prescribed burns are completed the same year as initiated (or within 1 year)
+pb.diffyr <- prescribed.burn %>% 
+  filter(init_y != comp_y) %>% 
+  mutate(diff_yr = as.numeric(comp_y) - as.numeric(init_y)) %>% 
+  select(Trt_ID, Init_Date, init_y, Comp_Date, comp_y, diff_yr) %>% 
+  arrange(diff_yr)
+
+#   Assign year to Trt_ID 73162
+trtid.73162.fix <- comp.date.na %>% 
+  filter(Trt_ID == 73162) %>% 
+  mutate(comp_y = "2020",
+         comp_est = "actual",
+         comp_date_est = as.Date("2020-02-22")) # confirmed and GIS version of table says 2020
+
+# Replace fixed NA estimated dates
+comp.date <- comp.date %>% 
+  filter(!is.na(comp_date_est)) %>% 
+  bind_rows(comp.date.na.fix, trtid.73162.fix) %>% 
+  arrange(Trt_ID)
+
+# Join with treatment.info
+treatment.info <- treatment.info %>% 
+  left_join(comp.date)
+
+
+
+# Look for negative values of time elapsed --------------------------------
+
+# Add column of days elapsed between treatment initiation and completion
+treatment.info <- treatment.info %>% 
+  mutate(init_comp_elapsed = comp_date_est - init_date_est)
+
+# Examine elapse of negative time
+neg.elapse <- treatment.info %>% 
+  filter(init_comp_elapsed < 0)
+
+
+## No dates est -----------------------------------------------------------
+
+# Negative time elapsed with no date estimates (original must be wrong)
+neg.elapse.actual <- neg.elapse %>% 
+  filter(init_est == "actual" & comp_est == "actual")
+#   Will just go with comp date given since it is the actual
+
+
+## Init date est ----------------------------------------------------------
+
+# Negative time elapsed with only initiation date estimated
+neg.elapse.init.est <- neg.elapse %>% 
+  filter(init_est != "actual" & comp_est == "actual")
+#   Will just go with comp date given since it is the actual
+
+
+# Comp date est -----------------------------------------------------------
+
+# Negative time elapsed with only completion date estimated
+neg.elapse.comp.est <- neg.elapse %>% 
+  filter(init_est == "actual" & comp_est != "actual") %>% 
+  select(Trt_ID, Init_Date, Comp_Date, Trt_Type_Sub, Treatment_Type, init_est, init_date_est, init_y, 
+         comp_est, comp_date_est, comp_y)
+
+# Check for matching years
+neg.elapse.comp.est$init_y == neg.elapse.comp.est$comp_y # all have same init & comp year
+
+# Replace est. comp date with actual init date
+neg.elapse.comp.est.fix <- neg.elapse %>% 
+  filter(init_est == "actual" & comp_est != "actual") %>% 
+  mutate(comp_date_est = init_date_est)%>% 
+  mutate(init_comp_elapsed = comp_date_est - init_date_est)
+
+
+## Both dates est ---------------------------------------------------------
+
+# Negative time elapsed with both dates estimated
+neg.elapse.both.est <- neg.elapse %>% 
+  filter(init_est != "actual" & comp_est != "actual") %>% 
+  select(Trt_ID, Init_Date, Comp_Date, Trt_Type_Sub, Treatment_Type, init_est, 
+         init_date_est, comp_est, comp_date_est, init_comp_elapsed)
+
+#   When comp day only is estimated (not month), retain estimated date
+#   When both comp and init day only (not month) are estimated, retain date
+
+# When comp month & day are estimated and only init day is estimated, replace est. comp date with init date
+neg.elapse.both.est.fix <- neg.elapse %>% 
+  filter(init_est == "estimated day" & comp_est == "estimated month and day") %>% 
+  mutate(comp_date_est = init_date_est)%>% 
+  mutate(init_comp_elapsed = comp_date_est - init_date_est)
+
+
+## Add in fixes -----------------------------------------------------------
+
+treatment.info <- treatment.info %>% 
+  filter(!Trt_ID %in% c(neg.elapse.both.est.fix$Trt_ID, neg.elapse.comp.est.fix$Trt_ID)) %>% 
+  bind_rows(neg.elapse.both.est.fix, neg.elapse.comp.est.fix) %>% 
+  arrange(Trt_ID)
+
+
+# Separate out columns for GIS join ---------------------------------------
+
+treatment.info.gisjoin <- treatment.info %>% 
+  select(Prj_ID, Trt_ID, init_date_est, comp_date_est, Trt_Type_Major, Trt_Type_Sub, Treatment_Type)
+
+
 
 # Write out as v0.0.1 -----------------------------------------------------
 
+write_csv(treatment.info,
+          file = "data/LTDL-versions/05.1_Treatment-info_v0.0.1.csv")
 
+write_csv(treatment.info.gisjoin,
+          file = "data/LTDL-versions/05.1_Treatment-info_v0.0.1gisjoin.csv")
+
+
+save.image("RData/05.1_treatment-info_v0.0.1.RData")
